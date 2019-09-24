03a97c4dbd885bd78d0e8e6ac8f1d921eb08b3c2
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2019-09-24 15:35:05 +0530

    Changes for Structure

---
+++ dao/db.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+/**
+ * This fle takes care only of Database operations and connectivity 
+ */
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: (process.env.ENVIRONMENT !== 'dev')
+}); 
+module.exports= {
+    query : (query, values, callback)=> {
+        // For Queries that dont need to supply values to execute
+        if( typeof(values) === 'function'){
+            callback = values;
+        }
+      pool.connect().then(client=>{
+        client.query(query,values, (errQuery, result )=>{
+          client.release();
+          if( errQuery){
+              console.error('Error While executing query ', {query,values});
+              return callback(errQuery);
+          }
+          else{
+            callback(null,result);
+          }
+        });
+      }).catch(err=>{
+        console.error('Error while establishing pool ');
+        return callback(err);
+      });
+   }
+};
