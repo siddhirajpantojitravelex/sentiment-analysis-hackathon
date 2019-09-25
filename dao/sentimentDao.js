8017aa1bbdf46a8d70141c4d209da85bb90d32a4
Atul Jadhav <Atul.Jadhav@travelex.com>
2019-09-25 18:49:52 +0530

    get data

--- dao/sentimentDao.js
+++ dao/sentimentDao.js
@@ -3,13 +3,13 @@ const db = require('./db');
 
 async function insertIntoSentimentTable(sentiment_info) {
     return new Promise((resolve, reject) => {
-        db.query(queries.SENTIMENT.INSERT, [sentiment_info.date, sentiment_info.title, sentiment_info.description, sentiment_info.jobTitle, sentiment_info.pros, sentiment_info.cons, sentiment_info.advice, 0, 0], (err, data) => {
+        db.query(queries.SENTIMENT.INSERT, [sentiment_info.date, sentiment_info.title, sentiment_info.description, sentiment_info.role, sentiment_info.pros, sentiment_info.cons, sentiment_info.advice, 0, 0], (err, data) => {
             if (err) {
                 reject(err)
             }
             else {
-                for (let i = 0; i < sentiment_info.classification.length; i++) {
-                    db.query(queries.SENTIMENT_CLASSIFICATION.INSERT, [data.rows[0].review_id, sentiment_info.classification[i].class,sentiment_info.classification[i].rating], (err, classificationData) => {
+                for (let i = 0; i < sentiment_info.classifications.length; i++) {
+                    db.query(queries.SENTIMENT_CLASSIFICATION.INSERT, [data.rows[0].review_id, sentiment_info.classifications[i].class,sentiment_info.classifications[i].rating], (err, classificationData) => {
                         if (err) {
                             reject(err)
                         }
--- service/crawler-service.js
+++ service/crawler-service.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 let Utils = require('../utils/Utils');
 
-function crawl() {
-    let util = new Utils('https://www.glassdoor.co.in/Reviews/Travelex-Reviews-E23898.htm');
-    util.process(results => {
+function crawl(url) {
+    console.log(url);
+    let util = new Utils(url);
+    util.process(url,(results) => {
         //console.log(results)
-        fs.writeFileSync("data.json", JSON.stringify(results), "UTF-8");
+        fs.appendFileSync("data.json", JSON.stringify(results), "UTF-8");
+        //fs.appendFile("data.json", JSON.stringify(results),(err)=>console.log(err));
     })
 }
-crawl()
+//for(let i=5;i<10;i++){
+    let u=`https://www.glassdoor.co.in/Reviews/Travelex-Reviews-E23898_P21.htm`
+    crawl(u)
+   async()=> await sleep(1000);
+//}
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
 module.exports = crawl;
